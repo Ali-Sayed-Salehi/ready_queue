bb40cffe204d2dbaee02e8a10587ba8c585b7dd2
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2023-01-06 16:36:31 -0500

    add queue package

---
+++ queue/LinkedQueue.java
@@ -0,0 +1,14 @@
+package queue;
+
+// Queue implementation class using a singly linked list
+public class LinkedQueue<E> implements NewQueue<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>(); // an empty list
+    public LinkedQueue() {} // new queue relies on the initially empty list
+    public int size() { return list.size(); }
+    public boolean isEmpty() { return list.isEmpty(); }
+    public void enqueue(E element) { list.addLast(element); }
+    public E first() { return list.first(); }
+    public E dequeue() { return list.removeFirst(); }
+}
+
+
